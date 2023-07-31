6f3481e327e9fa220878a7a13c408837bcde79fb
QuiiBz <tom.lienrd@gmail.com>
2023-07-31 09:16:48 +0200

    feat: basic rate limiter

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use crate::{
     routes::{full, ip, raw},
 };
 
-use axum::{routing::get, Router};
+use axum::{error_handling::HandleErrorLayer, http::StatusCode, routing::get, BoxError, Router};
 use handlebars::Handlebars;
 use maxminddb::Reader;
-use std::{net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 use tower_http::services::ServeDir;
 
 mod connect;
@@ -44,7 +45,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/raw", get(raw))
         .route("/:ip", get(ip))
         .nest_service("/static", ServeDir::new("public"))
-        .with_state(state);
+        .with_state(state)
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled error: {}", err),
+                    )
+                }))
+                .layer(BufferLayer::new(1024))
+                // TODO: use per-ip rate limiter instead of global
+                .layer(RateLimitLayer::new(10, Duration::from_secs(1))),
+        );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
